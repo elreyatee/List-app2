a3db6436302aab928b36b6a4aee4e0e5d1c47c8c
Ellery Temple Jr <elreyatee@gmail.com>
2015-04-25 11:52:18 -0700

    added first page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
   has_secure_password validations: false # supresses default validations
 
   validates :username, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: {
+    with: /(\w+)(@)(\w+)(.)(\w+)/, message: "invalid email"
+  } #checks email format ***@***.***
+
   validates :password, presence: true, on: :create, length: {minimum: 5}
 end
